f5aa5658cf8f9910dfd8692f2fea37246b9bc7b3
Elad Karni <elad.karni@gmail.com>
2025-11-11 19:20:47 -0500

    refactor: simplify Home component and remove FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-import { faqType } from "@/types/faqType";
-import FAQSection from "@/views/faq";
-
-type PostType = {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-};
-
 export default async function Home() {
-  const posts = await fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => data.filter((post: PostType) => post.id < 5));
-
-  const faqItems: faqType[] = posts.map((post: PostType) => ({
-    id: post.id.toString(),
-    question: post.title,
-    answer: post.body,
-  }));
-
   return (
     <main className="text-center max-w-[1100px] m-4 xl:mx-auto">
-      <FAQSection faqItems={faqItems} />
+      <div>
+        <h1>Test</h1>
+      </div>
     </main>
   );
 }
