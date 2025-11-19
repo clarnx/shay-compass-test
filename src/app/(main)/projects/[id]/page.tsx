79a70e4cfb048043d55eb1a6e7ce79a25d3f73df
Elad Karni <elad.karni@gmail.com>
2025-11-19 17:30:37 -0500

    fix: remove unused import of Link in ProjectPage component

--- src/app/(main)/projects/[id]/page.tsx
+++ src/app/(main)/projects/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { notFound } from 'next/navigation';
-import Link from 'next/link';
 import PropertyImageGallery from '@/components/PropertyImageGallery';
 import { getAllProjectSlugs, getProjectBySlug } from '@/util/payloadQueries';
 import { transformPayloadProject } from '@/util/transformPayloadData';
